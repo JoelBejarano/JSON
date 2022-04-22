c64c1bcc8d8120126131a8c193fbb92aa3c0a5dc
Joel Bejarano <jabej534@learner.dvusd.org>
2022-04-22 08:57:27 -0700

    .

--- Program.cs
+++ Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Console;
 
 namespace JSON
 {
-    class Program
+    namespace SerializeBasic
     {
-        static void Main(string[] args)
+        public class Program
         {
+            public static void Main()
+            {
+                var weatherForecast = new WeatherForecast
+                {
+                    Date = DateTime.Parse("2019-08-01"),
+                    TemperatureCelsius = 25,
+                    Summary = "Hot"
+                };
+
+                var PhoenixForecast = new WeatherForecast
+                {
+                    Date = DateTime.Parse("2019-08-01"),
+                    TemperatureCelsius = 28,
+                    Summary = "Hot"
+                };
+
+                string jsonString = JsonSerializer.Serialize(weatherForecast);
+                WriteLine(jsonString);
+
+                string PhoenixString = JsonSerializer.Serialize(PhoenixForecast);
+                WriteLine(PhoenixString);
+
+                WeatherForecast w = JsonSerializer.Deserialize<WeatherForecast>(PhoenixString);
+                WriteLine(w.Summary);
+
+                ReadKey();
+            }
+
+
         }
+
     }
 }
--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace JSON
 {
     class WeatherForecast
     {
         public DateTimeOffset Date { get; set; }
-        public int TempratureCelsius { get; set; }
+        public int TemperatureCelsius { get; set; }
         public string Summary { get; set; }
     }
 }
